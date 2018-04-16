71283fcc85ba86408557d4817c877129ed54b7b1
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2018-04-16 16:43:08 +0900

    edit edit.html.haml about flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
 
   def edit
+
   end
 
   def update
     if current_user.update(user_params)
+      flash[:notice] = '変更されました'
       redirect_to root_path
     else
+      flash[:alert] = '変更できませんでした'
       render :edit
     end
   end
