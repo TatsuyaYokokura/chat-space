9e3a9996bcc7bb365be4d62395a053c19310e14e
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2018-04-26 12:30:30 +0900

    edit sending function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,21 @@ class MessagesController < ApplicationController
     @groups = current_user.groups    # _aside.hmtlでも使用するためのインスタンス変数の設定
     @group = Group.find(params[:group_id])
     @current_group_users = @group.users
+    @message = Message.new
   end
+
+  def create
+    binding.pry
+    @group = Group.find(params[:group_id])
+    @message = @group.message.new(message_params)
+    @message.save
+    render :index
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:message, :image)
+  end
+
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  belongs_current_userto :user
+  # belongs_current_user_to :user
   belongs_to :group
   mount_uploader :image, ImageUploader
 end
