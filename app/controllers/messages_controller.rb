8042f0a2e0ae3f0af8bc5be6bac7310fbaaf4941
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2018-04-26 17:36:38 +0900

    Add message create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,19 +4,24 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @current_group_users = @group.users
     @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
-    binding.pry
     @group = Group.find(params[:group_id])
-    @message = @group.message.new(message_params)
-    @message.save
-    render :index
+    @message = @group.messages.new(message_params)
+    if @message.message.present?
+      @message.save
+      redirect_to group_messages_path(@group)
+    else
+      flash.row[:alert] = 'メッセージが入力されていません'
+      redirect_to group_messages_path(@group)
+    end
   end
 
   private
   def message_params
-    params.require(:message).permit(:message, :image)
+    params.require(:message).permit(:message, :image).merge(user_id: current_user.id)
   end
 
 
