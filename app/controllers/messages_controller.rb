4356393b6ad07e523058fb6f7c7f87d23b88ed11
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2018-04-26 20:53:52 +0900

    Edit group view & controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,21 @@
 class MessagesController < ApplicationController
+
+  before_action :current_group
+
   def index
     @groups = current_user.groups    # _aside.hmtlでも使用するためのインスタンス変数の設定
-    @group = Group.find(params[:group_id])
     @current_group_users = @group.users
     @message = Message.new
     @messages = @group.messages.includes(:user)
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @message = @group.messages.new(message_params)
-    if @message.message.present?
+    if @message.message?
       @message.save
       redirect_to group_messages_path(@group)
     else
-      flash.row[:alert] = 'メッセージが入力されていません'
-      redirect_to group_messages_path(@group)
+      redirect_to group_messages_path(@group), alert: 'メッセージを入力してください'
     end
   end
 
@@ -24,5 +24,8 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:message, :image).merge(user_id: current_user.id)
   end
 
+  def current_group
+    @group = Group.find(params[:group_id])
+  end
 
 end
