71b0f9ed6254d5f2f9142b271f00a60233293946
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2018-04-29 17:39:32 +0900

    Add vaidates about messega&image

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,12 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validates :message, presence: true
+  # validates :message_valid, presence: true
+  validates :message, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
+
+  # def message_valid
+  #   (message || image).present?
+  # end
+
 end
